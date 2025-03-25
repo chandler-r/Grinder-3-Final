86049a1414aea4de35349e41407868b043fdd3b7
Nora Fayyazuddin Ljungberg <nef33@cornell.edu>
2025-03-25 17:20:40 -0400

    Create hand interface
    - Added types, variables, and functions for Hand module
    - Updated implementation file with dummy functions for now

--- final_project/lib/hand.ml
+++ final_project/lib/hand.ml
@@ -1,1 +1,15 @@
-(*hello*)
+type t (* = some list or array of cards *)
+type hands = HighCard
+
+exception TooManyCards
+
+let hand_size = 7
+let play_limit = 5
+let hand_limit = 3
+let highest_hand x = HighCard
+
+(*TODO*)
+(* will need some pattern match on the number/value corresponding to each card
+   in the hand *)
+let discard cards hand = hand (*TODO*)
+let play cards hand = hand (*TODO*)
---
+++ final_project/lib/hand.mli
@@ -0,0 +1,34 @@
+type t
+(* hand.t is the type for representing hands, i.e. some collection of cards. *)
+
+type hands
+(* represents the different types of hand that can be played. *)
+
+exception TooManyCards
+(* raised if the player tries to play a hand with more cards than what is
+   allowed. *)
+
+(*Functionality:
+
+  check highest-scoring version of a collection of cards*)
+
+val hand_size : int
+(** [hand_size] is the maximum number of cards a player can have in their hand.
+*)
+
+val play_limit : int
+(** [play_limit] is the number of cards that a player can play at a time. *)
+
+val highest_hand : t -> hands
+(** [highest_score hand] is the highest score possible from a given set of
+    cards. Final implementation should output a tuple int * hand_type.t or just
+    a hand type, i.e. say whether the highest *)
+
+val discard : t -> t -> t
+(** [discard cards hand] discards a subset of cards in a hand. Should check that
+    the number of allowed discards is >0 (if we're allowing discards) and
+    replace them with cards from the deck, if available. *)
+
+val play : t -> t -> t
+(** [play cards hand] plays cards from the hand and replaces them with cards
+    from the deck, if available. [highest_hand] is a helper function for it. *)
