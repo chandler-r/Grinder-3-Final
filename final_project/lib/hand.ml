a48b14a0225a330d7a5a546ee1f1b7e7c2b6f1a3
Nora Fayyazuddin Ljungberg <nef33@cornell.edu>
2025-03-26 14:54:57 -0400

    Write hand.ml functions
    - Implemented several interface specifications in the Hand
    implementation file
    - Updated interface file based on new needs

--- final_project/lib/hand.ml
+++ final_project/lib/hand.ml
@@ -1,15 +1,49 @@
-type t (* = some list or array of cards *)
-type hands = HighCard
+type t = Card.t list
+(* AF: [card1; card2; ...; cardn] is a representation of a collection of cards, either held in hand or a subset of those to be played or discarded. *)
+(* RI: the length of the list representing a hand should be no more than hand_size. The length of the list representing cards played or discarded should be no more than play_limit *)
+
+type hands =
+  | HighCard
+  | Pair
+  | TwoPair
+  | ThreeKind
+  | Straight
+  | Flush
+  | FullHouse
+  | FourKind
+  | StraightFlush
+  | RoyalFlush
 
 exception TooManyCards
 
-let hand_size = 7
-let play_limit = 5
-let hand_limit = 3
-let highest_hand x = HighCard
+let hand_size = ref 7
+let play_limit = ref 5
+let hands_per_round = ref 3
 
+let rep_ok_hand hand =
+  if List.length hand <= !hand_size then hand else raise TooManyCards
+
+let rep_ok_play play =
+  if List.length play <= !play_limit then play else raise TooManyCards
+
+let highest_hand play = (HighCard, play)
 (*TODO*)
 (* will need some pattern match on the number/value corresponding to each card
    in the hand *)
-let discard cards hand = hand (*TODO*)
-let play cards hand = hand (*TODO*)
+
+let cycle_cards cards hand =
+  let len = List.length cards in
+  List.filter (Fun.negate (fun y -> List.mem y cards)) hand
+(*@ Deck.draw len*)
+(*TODO: will need to use some Deck.draw function where we draw [len] cards if
+  possible*)
+
+let discard cards hand =
+  let x = rep_ok_hand cards in
+  cycle_cards x hand
+
+let play cards hand =
+  let x = rep_ok_play cards in
+  (cycle_cards x hand, highest_hand cards)
+
+let to_list cards = cards
--- final_project/lib/hand.mli
+++ final_project/lib/hand.mli
@@ -12,27 +12,31 @@ exception TooManyCards
 
   check highest-scoring version of a collection of cards*)
 
-val hand_size : int
+val hand_size : int ref
 (** [hand_size] is the maximum number of cards a player can have in their hand.
 *)
 
-val play_limit : int
+val play_limit : int ref
 (** [play_limit] is the number of cards that a player can play at a time. *)
 
-val hand_limit : int
-(** [hand_limit] is the maximum number of hands that can be played in a given
-    round of the game*)
+val hands_per_round : int ref
+(** [hands_per_round] is the maximum number of hands that can be played in a
+    given round of the game*)
 
-val highest_hand : t -> hands
-(** [highest_score hand] is the highest score possible from a given set of
-    cards. Final implementation should output a tuple int * hand_type.t or just
-    a hand type, i.e. say whether the highest *)
+val highest_hand : t -> hands * t
+(** [highest_hand hand] is the highest hand type possible from a given set of
+    cards, along with the cards that will be used for scoring. *)
 
 val discard : t -> t -> t
 (** [discard cards hand] discards a subset of cards in a hand. Should check that
     the number of allowed discards is >0 (if we're allowing discards) and
     replace them with cards from the deck, if available. *)
 
-val play : t -> t -> t
+val play : t -> t -> t * (hands * t)
 (** [play cards hand] plays cards from the hand and replaces them with cards
-    from the deck, if available. [highest_hand] is a helper function for it. *)
+    from the deck, if available. [highest_hand] is a helper function for it.
+    Returns the new hand, the highest possible corresponding hand type of the
+    played cards, and the played cards themselves for scoring. *)
+
+val to_list : t -> Card.t list
+(** [to_list cards] returns a list of cards in hand. *)
