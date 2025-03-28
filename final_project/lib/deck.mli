4b561c96edce62842137b9f720b116eec7924f4f
ril33 <ril33@cornell.edu>
2025-03-28 01:07:56 -0400

    Fixed signatures

--- final_project/lib/deck.mli
+++ final_project/lib/deck.mli
@@ -1,3 +1,5 @@
+open Card
+
 type t
 
 exception NotInDeck
@@ -8,19 +10,19 @@ val init : unit -> t
 (** [init ()] initializes a 52 card deck. Different deck types can have
     different makeups of cards *)
 
-val add_card : t -> Card.t -> unit
+val add_card : t -> Card.t -> t
 (** [add_card c] adds the card c into the deck. This means decks are allowed to
     go above 52 cards. *)
 
-val add_modifier : t -> Card.t -> unit
+val add_modifier : t -> Card.t -> t
 (** TODO: Wait till later, might have to modify the card module in order to
     accomodate this, stretch goal. *)
 
-val remove_card : t -> Card.t -> unit
+val remove_card : t -> Card.t -> t
 (** [remove card c] removes c from the deck. The card has to be in the deck,
     raises NotInDeck exception if not. This means decks are allowed to go under
     52 cards. *)
 
-val draw_cards : int -> Card.t list
+val draw_cards : t -> int -> Card.t list
 (** [draw_cards n] draws n cards and returns them in a list. The cards are drawn
     at random. *)
