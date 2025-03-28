e1a7c0aa03470431f31178d7f04e5b40acaf4fc6
ril33 <ril33@cornell.edu>
2025-03-28 01:08:31 -0400

    Implemented all of the functions specified in the .mli file

--- final_project/lib/deck.ml
+++ final_project/lib/deck.ml
@@ -1,1 +1,44 @@
-type t = card array
+open Card
+
+type t = Card.t array
+
+exception NotInDeck
+
+(** Abstract Function (AF): A deck [d] is represented as an array of cards of
+    type [Card.t]. The deck is viewed as an unordered collection of cards and
+    may containt any number of cards (including duplicates). *)
+
+let init () : t =
+  let suits = [ Card.Spades; Hearts; Diamonds; Clubs ] in
+  let ranks = [ 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14 ] in
+  Array.of_list
+    (List.concat
+       (List.map
+          (fun s -> List.map (fun r -> Card.create_card s r) ranks)
+          suits))
+
+let add_card deck card = Array.append deck [| card |]
+let add_modifier deck card = failwith "Not implemented"
+
+let remove_card deck card =
+  let lst = Array.to_list deck in
+  (* Helper function to remove the first instance of card. *)
+  let rec remove = function
+    | [] -> raise NotInDeck
+    | h :: t -> if h = card then t else h :: remove t
+  in
+  Array.of_list (remove lst)
+
+let draw_cards deck n =
+  let deck_copy = Array.copy deck in
+  let current_length = ref (Array.length deck_copy) in
+  let drawn_cards = ref [] in
+  for _ = 1 to n do
+    if !current_length = 0 then failwith "Not enough cards in the deck";
+    let r = Random.int !current_length in
+    let card = deck_copy.(r) in
+    drawn_cards := card :: !drawn_cards;
+    deck_copy.(r) <- deck_copy.(!current_length - 1);
+    current_length := !current_length - 1
+  done;
+  List.rev !drawn_cards
