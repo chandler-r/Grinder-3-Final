231678709080a0216ae17c65d833ff6c7ce4c30e
ril33 <ril33@cornell.edu>
2025-03-28 01:09:00 -0400

    Exposed suit and added a create_card signatutre for the deck.ml file

--- final_project/lib/card.mli
+++ final_project/lib/card.mli
@@ -3,8 +3,20 @@ type t
     have a suit which is one of Spades, Hearts, Diamonds, or Clubs. The rank of
     the card will range from 2–10, Jack, Queen, King, or Ace. *)
 
+type suit =
+  | Spades
+  | Hearts
+  | Diamonds
+  | Clubs
+      (** [suit] represents the suit of the card, of which could be Spades,
+          Hearts, Diamonds, or Clubs. *)
+
 exception BadCard
 (** [BadCard] is raised when a card is not a part of a standard deck. *)
+
+val create_card : suit -> int -> t
+(** [create_card s n] creates a card with suit [s] and rank [n]. Raises
+    [BadCard] if [n] is not in the range of 1 to 14. *)
 
 val of_pair : string * int -> t
 (** [of_pair (s, n)] is a card with suite [s] and rank [n]. Raises
