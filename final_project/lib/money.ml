3a2405383498da38473555f403d8f7cf75bb829c
Nora Fayyazuddin Ljungberg <nef33@cornell.edu>
2025-04-20 08:10:29 -0400

    Added money module
    - Introduces a ref representing the player's money
    - Added function for calculating end-of-round payouts
    - Added temporary blind functionality which should be moved later

---
+++ final_project/lib/money.ml
@@ -0,0 +1,23 @@
+let money = ref 4
+
+type blind =
+  | Small
+  | Big
+  | Boss
+
+exception InsufficientFunds
+
+let interest_cap = ref 5
+let pay x = if !money >= x then money := !money - x else raise InsufficientFunds
+
+let end_of_round blind hands =
+  let x =
+    match blind with
+    | Small -> 3
+    | Big -> 4
+    | Boss -> 5
+  in
+  money := !money + x + hands;
+  let interest = !money / 5 in
+  if interest < !interest_cap then money := !money + interest
+  else money := !money + !interest_cap
---
+++ final_project/lib/money.mli
@@ -0,0 +1,17 @@
+val money : int ref
+(** The mutable amount of money that the player has throughout the game *)
+
+exception InsufficientFunds
+
+type blind
+(** Will not be in the final implementation, but it's needed to calculate
+    end-of-round payouts and I'm not sure where it will be defined. *)
+
+val pay : int -> unit
+(** [pay x] reduces [money] by an amount [x]. Fails if [x > !money]. *)
+
+val end_of_round : blind -> int -> unit
+(** [end_of_round blind hands] adjusts money for the end of the round. Small,
+    big, and boss blinds give $3, $4, and $5 respectively. Players earn up to $5
+    of interest based on how much money they have in hand at the end of the
+    round and earn $1 for each remaining hand at the end of the round. *)
